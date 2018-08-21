7ddcb993f3c44242ad9253f341c1b8590a4c9bc4
gerryRcom <gerryr@gerryr.com>
2018-08-21 20:55:30 +0100

    Initial commit

---
+++ Nodes.mm
@@ -0,0 +1,10 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1533285841586" ID="ID_776593647" MODIFIED="1533294538133" TEXT="Nodes">
+<node CREATED="1533294539216" ID="ID_358123168" MODIFIED="1533294547287" POSITION="right" TEXT="Receiver">
+<node CREATED="1533298370911" ID="ID_506113636" MODIFIED="1533298388580" TEXT="Rsync (verifies - reference this)"/>
+</node>
+<node CREATED="1533294547887" ID="ID_103969375" MODIFIED="1533294549957" POSITION="right" TEXT="Cache"/>
+<node CREATED="1533294550184" ID="ID_921395811" MODIFIED="1533294552817" POSITION="right" TEXT="Transmit"/>
+</node>
+</map>
