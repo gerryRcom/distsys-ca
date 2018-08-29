26c6358250f21eaf7724c37376dc6dc8ad987d74
gerryRcom <gerryr@gerryr.com>
2018-08-29 22:30:19 +0100

    Event exporting code working, initial node lookup code

--- code/client.ps1
+++ code/client.ps1
@@ -1,2 +1,23 @@
-$txtRecords = nslookup -type=TXT gerryr.com 8.8.8.8
-Write-Host $txtRecord
+#Create log directory if it doesn't exist
+New-Item -ItemType Directory -Force -Path c:\_IT\log | Out-Null
+#Get all required events for the past 6 hours from the Application log
+$events = Get-EventLog -LogName Application -After (Get-Date).AddHours(-6) | Where-Object {$_.Source -eq "Microsoft-Windows-Backup"}
+#Export all required events to individual .xmls files. Ensure unique name by generating a GUID for each filename
+Foreach ($event in $events)
+{
+    $guid = New-Guid
+    $logName = "c:\_IT\log\" + $env:computername + "$" + $guid + ".xml"
+    $event | Export-Clixml -Path $logName
+}
+
+$txtRecords = Resolve-DnsName gerryr.com -Type TXT
+$splitter = "."
+Foreach ($record in $txtRecords)
+{
+    $content = $record.Strings
+    If ($content.SubString(0,4) -eq 'zoho')
+    {
+        Write-Host $content.Split($splitter)
+    }
+
+}
