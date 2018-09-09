a461b6a060d45259f9b9c2a9b5c7d9811f3efa00
gerryRcom <gerryr@gerryr.com>
2018-09-09 11:29:26 +0100

    Switched to beautifulsoup for xml parsing of log files

--- code/receiver-node.py
+++ code/receiver-node.py
@@ -1,7 +1,22 @@
 import os
-import xml.etree.ElementTree as ET
+from bs4 import BeautifulSoup
+#import xml.etree.ElementTree as ET
+#from lxml import etree
 for file in os.listdir("/home/log-transfer/incoming-logs/"):
     if file.endswith(".xml"):
-        #print(file)
-        currentLog = ET.parse(file)
-        print(currentLog)
+        fileInput = open(file, "r")
+        fileContent = fileInput.read()
+        soup = BeautifulSoup(fileContent, "xml")
+        machineName = soup.find('S', {'N': 'MachineName'})
+        eventID = soup.find('I32', {'N': 'EventID'})
+        eventSource = soup.find('S', {'N': 'Source'})
+        message = soup.find('S', {'N': 'Message'})
+        eventData = soup.find('Obj', {'N': 'EntryType'})
+        eventLevel = eventData.findChildren('I32')
+        eventTime = soup.find('DT', {'N': 'TimeGenerated'})
+        print(eventID.string)
+        print(message.string)
+        print(machineName.string)
+        print(eventLevel[0].text)
+        print(eventSource.string)
+        print(eventTime.string)
