555914bec527d6f39d6d8ad97ab480523ef10207
gerryRcom <gerryr@gerryr.com>
2018-09-11 20:11:38 +0100

    Tidied up content parsing. Got DB insert working

--- code/receiver-node.py
+++ code/receiver-node.py
@@ -9,42 +9,47 @@ for file in os.listdir("/home/log-transfer/incoming-logs/"):
         fileInput = open(file, "r")
         fileContent = fileInput.read()
         soup = BeautifulSoup(fileContent, "xml")
-        machineName = soup.find('S', {'N': 'MachineName'})
-        eventID = soup.find('I32', {'N': 'EventID'})
-        eventSource = soup.find('S', {'N': 'Source'})
-        message = soup.find('S', {'N': 'Message'})
+        machineName = str(soup.find('S', {'N': 'MachineName'}).string)
+        eventID = int(soup.find('I32', {'N': 'EventID'}).string)
+        eventSource = str(soup.find('S', {'N': 'Source'}).string)
+        message = str(soup.find('S', {'N': 'Message'}).string)
         eventData = soup.find('Obj', {'N': 'EntryType'})
-        eventLevel = eventData.findChildren('I32')
-        eventTime = soup.find('DT', {'N': 'TimeGenerated'})
+        eventLevel = int(eventData.findChildren('I32')[0].text)
+        eventTime = str(soup.find('DT', {'N': 'TimeGenerated'}).string)
         log_guid = (file.split('$')[1].split('.')[0])
-        print(eventID.string)
-        print(message.string)
-        print(machineName.string)
-        print(eventLevel[0].text)
-        print(eventSource.string)
-        print(eventTime.string)
-        print(log_guid)
+        #print(eventID)
+        #print(message)
+        #print(machineName)
+        #print(eventLevel)
+        #print(eventSource)
+        #print(eventTime)
+        #print(log_guid)
 
-#Check if SQL port is open on DB server before attempting transmit (src: stackoverflow)
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sock.settimeout(5)
-result = sock.connect_ex(('SERVER',3306))
-if result == 0:
-        print "Port is open"
-        db_connect = mysql.connector.connect(host="SERVER",user="ENTERUSER",passwd="ENTERPASSWORD",database="DATABASENAME")
-        db_cursor = db_connect.cursor()
+        #Check if SQL port is open on DB server before attempting transmit (src: stackoverflow)
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.settimeout(5)
+        result = sock.connect_ex(('10.166.0.2',3306))
+        #If DB connection is sucessful, insert the parsed data into the database
+        if result == 0:
+                #print "Port is open"
+                db_connect = mysql.connector.connect(host="SERVER",user="DATABASEUSER",passwd="ENTERPASSWORD",database="DATABASENAME")
+                db_cursor = db_connect.cursor()
 
-        insert_command = "INSERT INTO log_data (log_guid, machine_name, event_src, event_id, event_level, event_msg) VALUES (%s, %s, %s, %s, %s, %s)"
-        insert_data = (log_guid, machineName.string, eventSource.string, int(eventID.string), int(eventLevel[0].text), message.string)
-        #try:
-        db_cursor.execute(insert_command,insert_data)
-        db_connect.comit()
-        db_connect.close()
-        print("insert ran")
-        #except:
-        #       db_connect.rollback()
-        #       db_connect.close()
-        #       print("insert did not run")
-#else:
-        print "Port is not open"
-        exit()
+                insert_command = "INSERT INTO log_data (log_guid, machine_name, event_src, event_id, event_level, event_msg) VALUES (%s, %s, %s, %s, %s, %s)"
+                insert_data = (log_guid, machineName, eventSource, eventID, eventLevel, message)
+                #Check for any error during DB insert, if an error returns cancel the insert
+                try:
+                        db_cursor.execute(insert_command,insert_data)
+                        db_connect.commit()
+                        db_connect.close()
+                        #print("insert ran")
+                        #Given DB insert for this file has suceeded, transfer current log file to log-archive sub-folder
+                        #os.rename(file, "/home/log-transfer/incoming-logs/log-archives/")
+                except:
+                        db_connect.rollback()
+                        db_connect.close()
+                        #print("insert did not run")
+        #If DB connection is not sucessful, exit the script.  It will reattempt on next cron run
+        else:
+                print "Port is not open"
+                exit()
